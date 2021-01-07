2cc7c584739f9f6ddedfffc541e8d686b43cde26
Jim Clausing <clausing@computer.org>
2021-01-07 16:34:16 -0500

    Clean up formatting with black

--- docker-mount.py
+++ docker-mount.py
@@ -2,14 +2,14 @@
 #
 #  Title: 	docker-mount.py
 #  Author:	Jim Clausing
-#  Date:	2019-01-30
-#  Version:	1.0.1
+#  Date:	2021-01-07
+#  Version:	1.0.2
 #
 #  Purpose:	Allow the mounting of the AUFS layered/union filesystem from
-#		a docker container to be mounted (read-only) for the purposes
-#		of forensic examination
+# 		a docker container to be mounted (read-only) for the purposes
+# 		of forensic examination
 #
-# Copyright (c) 2016-2019 AT&T Open Source. All rights reserved.
+# Copyright (c) 2016-2021 AT&T Open Source. All rights reserved.
 #
 
 from sys import *
@@ -18,68 +18,93 @@ import argparse
 from subprocess import call
 import json
 
+
 def aufs_mount():
-    layers = open(layerid).read().split('\n')
+    layers = open(layerid).read().split("\n")
     layers = layers[:-1]
-    layers.insert(0,layerid)
+    layers.insert(0, layerid)
 
-    elements = [args.root+args.path+'/'+args.storage+'/diff/'+s for s in layers]
+    elements = [args.root + args.path + "/" + args.storage + "/diff/" + s for s in layers]
 
-    f=":".join(elements)
+    f = ":".join(elements)
 
-    call(["/bin/mount","-t","aufs","-r","-o","br:"+f,"none",args.mntpnt], shell=False)
-    return()
+    call(["/bin/mount", "-t", "aufs", "-r", "-o", "br:" + f, "none", args.mntpnt], shell=False)
+    return ()
+
 
 def overlay2_mount():
-    lowerdir = open(args.root + args.path + '/' + args.storage + '/' + layerid + '/lower').read().rstrip()
-    
+    lowerdir = open(args.root + args.path + "/" + args.storage + "/" + layerid + "/lower").read().rstrip()
+
     os.chdir(dockerpath)
 
-    call(["/bin/mount","-t","overlay","overlay","-r","-o","lowerdir="+lowerdir+",upperdir="+layerid+"/diff,workdir="+layerid+"/work",args.mntpnt], shell=False)
-    return()
+    call(
+        [
+            "/bin/mount",
+            "-t",
+            "overlay",
+            "overlay",
+            "-r",
+            "-o",
+            "lowerdir=" + lowerdir + ",upperdir=" + layerid + "/diff,workdir=" + layerid + "/work",
+            args.mntpnt,
+        ],
+        shell=False,
+    )
+    return ()
 
 
-__version_info__ = (1,0,1)
+__version_info__ = (1, 0, 2)
 __version__ = ".".join(map(str, __version_info__))
 
-parser = argparse.ArgumentParser(prog='docker-mount', description='Mount docker container filesystem for forensic examination')
-parser.add_argument('container', help='container id (sha256 hex string)')
-parser.add_argument('mntpnt', help='mount point where read-only filesystem will be mounted')
-#parser.add_argument('--verbose','-v', action='store_true', help='verbose',)
-parser.add_argument('-V','--version', action='version', help='print version number', version='%(prog)s v' + __version__)
-parser.add_argument('--root','-r', 
-        help='root of filesystem containing forensic image (should include trailing /, e.g. /mnt/image/) [default: none]', default='')  # e.g., /mnt/image/
-parser.add_argument('--path','-p', help='path to docker files [default: /var/lib/docker]', default='/var/lib/docker')
-parser.add_argument('--storage','-s', help='storage driver, currently aufs and overlay2 are supported [default: aufs]', default='aufs')
+parser = argparse.ArgumentParser(
+    prog="docker-mount", description="Mount docker container filesystem for forensic examination"
+)
+parser.add_argument("container", help="container id (sha256 hex string)")
+parser.add_argument("mntpnt", help="mount point where read-only filesystem will be mounted")
+# parser.add_argument('--verbose','-v', action='store_true', help='verbose',)
+parser.add_argument(
+    "-V", "--version", action="version", help="print version number", version="%(prog)s v" + __version__
+)
+parser.add_argument(
+    "--root",
+    "-r",
+    help="root of filesystem containing forensic image (should include trailing /, e.g. /mnt/image/) [default: none]",
+    default="",
+)  # e.g., /mnt/image/
+parser.add_argument("--path", "-p", help="path to docker files [default: /var/lib/docker]", default="/var/lib/docker")
+parser.add_argument(
+    "--storage", "-s", help="storage driver, currently aufs and overlay2 are supported [default: aufs]", default="aufs"
+)
 
-args=parser.parse_args()
+args = parser.parse_args()
 
-dockerroot = args.root+args.path
-config1 = dockerroot + '/containers/' + args.container + '/config.json'
-config2 = dockerroot + '/containers/' + args.container + '/config.v2.json'
-if (os.path.isfile(config1)):
+dockerroot = args.root + args.path
+config1 = dockerroot + "/containers/" + args.container + "/config.json"
+config2 = dockerroot + "/containers/" + args.container + "/config.v2.json"
+if os.path.isfile(config1):
     dockerversion = 1
-elif (os.path.isfile(config2)):
+elif os.path.isfile(config2):
     dockerversion = 2
 else:
-    raise Exception('Unknown docker version or invalid container id, check and try again')
+    raise Exception("Unknown docker version or invalid container id, check and try again")
 
 
-if (dockerversion == 1):
+if dockerversion == 1:
     layerid = args.container
 else:
-    layerid = open(args.root + args.path + '/image/' + args.storage + '/layerdb/mounts/' + args.container + '/mount-id').read()
+    layerid = open(
+        args.root + args.path + "/image/" + args.storage + "/layerdb/mounts/" + args.container + "/mount-id"
+    ).read()
 # image/aufs/layerdb/mounts/516ae11fdca97f3228dac2dea2413c6d34a444e24b1d8b2a47cd54fbca091905/mount-id
-    
-if (args.storage == 'aufs'):
-    dockerpath=args.root+args.path+'/'+args.storage+'/layers'
-elif (args.storage == 'overlay2'):
-    dockerpath=args.root+args.path+'/'+args.storage
+
+if args.storage == "aufs":
+    dockerpath = args.root + args.path + "/" + args.storage + "/layers"
+elif args.storage == "overlay2":
+    dockerpath = args.root + args.path + "/" + args.storage
 
 os.chdir(dockerpath)
 
-if (args.storage == 'aufs'):
+if args.storage == "aufs":
     aufs_mount()
-elif (args.storage == 'overlay2'):
+elif args.storage == "overlay2":
     overlay2_mount()
-
--- mac-robber.py
+++ mac-robber.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 #
 # Author:  Jim Clausing
-# Date:    2019-03-13
-# Version: 1.3.0
+# Date:    2021-01-07
+# Version: 1.3.1
 #
 # Desc: rewrite of the sleithkit mac-robber in Python
 # Unlinke the TSK version, this one can actually includes the MD5 & inode number
@@ -12,7 +12,7 @@
 # Note: in Python 2.7.x, st_ino, st_dev, st_nlink, st_uid, and st_gid are dummy variables
 # on Windows systems. This is apparently fixed in current Python 3 versions.
 # On *ALL* systems, os.stat does not return btime, so we put 0 there. :-(
-# 
+#
 # A useful way to use this on a live Linux system is with read-only --bind mounts
 #
 #   # mount --bind / /mnt
@@ -21,7 +21,7 @@
 #
 # This gets us hashes, but because the bind mount is read-only doesn't update atimes
 #
-# Copyright (c) 2017-2019 AT&T Open Source. All rights reserved.
+# Copyright (c) 2017-2021 AT&T Open Source. All rights reserved.
 #
 
 from __future__ import print_function
@@ -31,51 +31,53 @@ import argparse
 import hashlib
 from stat import *
 
-__version_info__ = (1,3,0)
+__version_info__ = (1, 3, 1)
 __version__ = ".".join(map(str, __version_info__))
 
+
 def mode_to_string(mode):
-    lookup = ['---','--x','-w-','-wx','r--','r-x','rw-','rwx']
+    lookup = ["---", "--x", "-w-", "-wx", "r--", "r-x", "rw-", "rwx"]
     if S_ISDIR(mode):
-        mode_str = 'd'
+        mode_str = "d"
     elif S_ISCHR(mode):
-        mode_str = 'c'
+        mode_str = "c"
     elif S_ISBLK(mode):
-        mode_str = 'b'
+        mode_str = "b"
     elif S_ISREG(mode):
-        mode_str = '-'
+        mode_str = "-"
     elif S_ISFIFO(mode):
-        mode_str = 'p'
+        mode_str = "p"
     elif S_ISLNK(mode):
-        mode_str = 'l'
+        mode_str = "l"
     elif S_ISSOCK:
-        mode_str = 's'
-    own_mode = lookup[(mode & 0o700)>>6]
+        mode_str = "s"
+    own_mode = lookup[(mode & 0o700) >> 6]
     if mode & 0o4000:
         if mode & 0o100:
-            own_mode = own_mode.replace('x','s')
+            own_mode = own_mode.replace("x", "s")
         else:
-            own_mode = own_mode[:1] + 'S'
+            own_mode = own_mode[:1] + "S"
     mode_str = mode_str + own_mode
-    grp_mode = lookup[(mode & 0o70)>>3]
+    grp_mode = lookup[(mode & 0o70) >> 3]
     if mode & 0o2000:
         if mode & 0o10:
-            grp_mode = grp_mode.replace('x','s')
+            grp_mode = grp_mode.replace("x", "s")
         else:
-            grp_mode = grp_mode[:1] + 'S'
+            grp_mode = grp_mode[:1] + "S"
     mode_str = mode_str + own_mode
     oth_mode = lookup[(mode & 0o7)]
     if mode & 0o1000:
         if mode & 0o1:
-            oth_mode = oth_mode.replace('x','t')
+            oth_mode = oth_mode.replace("x", "t")
         else:
-            oth_mode = oth_mode[:1] + 'T'
+            oth_mode = oth_mode[:1] + "T"
     mode_str = mode_str + oth_mode
     return mode_str
 
-def process_item(dirpath,item):
+
+def process_item(dirpath, item):
     md5 = hashlib.md5()
-    fname = os.path.join(dirpath,item)
+    fname = os.path.join(dirpath, item)
     if args.exclude and (fname in args.exclude or dirpath in args.exclude):
         return
     try:
@@ -89,13 +91,13 @@ def process_item(dirpath,item):
         return
     if args.hashes and S_ISREG(status.st_mode):
         try:
-            if not (fname.find('/proc/') != -1 and fname.endswith('/kcore')) and status.st_size > 0:
+            if not (fname.find("/proc/") != -1 and fname.endswith("/kcore")) and status.st_size > 0:
                 with open(fname, "rb") as f:
                     for block in iter(lambda: f.read(65536), b""):
                         md5.update(block)
                 md5str = md5.hexdigest()
             elif status.st_size == 0:
-                md5str = "d41d8cd98f00b204e9800998ecf8427e" 
+                md5str = "d41d8cd98f00b204e9800998ecf8427e"
             else:
                 md5str = "0"
         except IOError:
@@ -104,15 +106,15 @@ def process_item(dirpath,item):
         md5str = "0"
     mode = mode_to_string(status.st_mode)
     if os.path.islink(fname) and status.st_size > 0:
-        mode = mode + ' -> ' + os.readlink(fname)
-    if sys.version_info<(2,7,0):
-        mtime = '%14.3f' % (status.st_mtime)
-        atime = '%14.3f' % (status.st_atime)
-        ctime = '%14.3f' % (status.st_mtime)
+        mode = mode + " -> " + os.readlink(fname)
+    if sys.version_info < (2, 7, 0):
+        mtime = "%14.3f" % (status.st_mtime)
+        atime = "%14.3f" % (status.st_atime)
+        ctime = "%14.3f" % (status.st_mtime)
     else:
-        mtime = '{:14.3f}'.format(status.st_mtime)
-        atime = '{:14.3f}'.format(status.st_atime)
-        ctime = '{:14.3f}'.format(status.st_mtime)
+        mtime = "{:14.3f}".format(status.st_mtime)
+        atime = "{:14.3f}".format(status.st_atime)
+        ctime = "{:14.3f}".format(status.st_mtime)
     btime = 0
     size = status.st_size
     uid = status.st_uid
@@ -120,42 +122,83 @@ def process_item(dirpath,item):
     inode = status.st_ino
     if args.rmprefix:
         if fname.startswith(args.rmprefix):
-            fname = fname[len(args.rmprefix):]
+            fname = fname[len(args.rmprefix) :]
     if args.prefix:
-        if fname.find('/') == 0:
+        if fname.find("/") == 0:
             fname = args.prefix + fname
         else:
-            fname = args.prefix + '/' + fname
-    return md5str+'|'+fname+'|'+str(inode)+'|'+mode+'|'+str(uid)+'|'+str(gid)+'|'+str(size)+'|'+atime+'|'+mtime+'|'+ctime+'|'+str(btime)
-    
+            fname = args.prefix + "/" + fname
+    return (
+        md5str
+        + "|"
+        + fname
+        + "|"
+        + str(inode)
+        + "|"
+        + mode
+        + "|"
+        + str(uid)
+        + "|"
+        + str(gid)
+        + "|"
+        + str(size)
+        + "|"
+        + atime
+        + "|"
+        + mtime
+        + "|"
+        + ctime
+        + "|"
+        + str(btime)
+    )
 
-if sys.version_info<(2,6,0):
+
+if sys.version_info < (2, 6, 0):
     sys.stderr.write("Not tested on versions earlier than 2.6\n")
     exit(1)
 
-parser = argparse.ArgumentParser(description='collect data on files')
-parser.add_argument('directories', metavar='DIR', nargs='+', help='directories to traverse')
-parser.add_argument('-m','--prefix', metavar='PREFIX', help='prefix string')
-parser.add_argument('-5','--hashes', action='store_true', help='do MD5 calculation (disabled by default)', default=False)
-parser.add_argument('-x','--exclude', metavar='EXCLUDE', action='append', help='directory trees or files to exclude, does not handle file extensions or regex', default=[])
-parser.add_argument('-r','--rmprefix', metavar='RMPREFIX', help='prefix to remove, useful when using read-only --bind mount to prevent atime updates')
-parser.add_argument('-V','--version',  action='version', help='print version number',
-                    version='%(prog)s v{version}'.format(version= __version__))
+parser = argparse.ArgumentParser(description="collect data on files")
+parser.add_argument("directories", metavar="DIR", nargs="+", help="directories to traverse")
+parser.add_argument("-m", "--prefix", metavar="PREFIX", help="prefix string")
+parser.add_argument(
+    "-5", "--hashes", action="store_true", help="do MD5 calculation (disabled by default)", default=False
+)
+parser.add_argument(
+    "-x",
+    "--exclude",
+    metavar="EXCLUDE",
+    action="append",
+    help="directory trees or files to exclude, does not handle file extensions or regex",
+    default=[],
+)
+parser.add_argument(
+    "-r",
+    "--rmprefix",
+    metavar="RMPREFIX",
+    help="prefix to remove, useful when using read-only --bind mount to prevent atime updates",
+)
+parser.add_argument(
+    "-V",
+    "--version",
+    action="version",
+    help="print version number",
+    version="%(prog)s v{version}".format(version=__version__),
+)
 
 args = parser.parse_args()
 
 for directory in args.directories:
-    for dirpath,dirs,files in os.walk(directory):
+    for dirpath, dirs, files in os.walk(directory):
         dirs[:] = [d for d in dirs if d not in args.exclude]
         for directory in dirs:
-            outstr = process_item(dirpath,directory)
+            outstr = process_item(dirpath, directory)
             if outstr is not None:
-                print (outstr)
+                print(outstr)
                 sys.stdout.flush()
         for filename in files:
             if filename in args.exclude:
                 continue
-            outstr = process_item(dirpath,filename)
+            outstr = process_item(dirpath, filename)
             if outstr is not None:
-                print (outstr)
+                print(outstr)
                 sys.stdout.flush()
